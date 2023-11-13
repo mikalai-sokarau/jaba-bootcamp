3e920db8b56bca272ec086546cef1b40b296a594
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-11-13 19:18:06 +0100

    feat: add module-info

---
+++ jmp/jmp-cloud-bank-impl/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module com.jmp.msokarau.jmp.cloud.bank.impl {
+  requires com.jmp.msokarau.jmp.bank.api;
+}
---
+++ jmp/jmp-dto/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.jmp.msokarau.jmp.dto {
+  exports com.jmp.msokarau.classes;
+  exports com.jmp.msokarau.constants;
+}
