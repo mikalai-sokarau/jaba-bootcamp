9876c874f325c192059c4b89e5f62d76fe6e37ce
Mikalai Sokarau <mikalaisokarau@gmail.com>
2023-11-18 13:40:51 +0100

    refactor: project structure

--- jmp/jmp-application/src/main/java/dev/msokarau/application/Application.java
+++ jmp/jmp-application/src/main/java/dev/msokarau/application/Application.java
@@ -1,8 +1,21 @@
 package dev.msokarau.application;
 
+import dev.msokarau.dto.User;
+import java.util.ArrayList;
+
 public class Application {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        ArrayList<User> users = createUsers();
+        System.out.println("Users: " + users);
+    }
+
+    private static ArrayList<User> createUsers() {
+        ArrayList<User> users = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            users.add(new User("User" + (i + 1)));
+        }
+
+        return users;
     }
 }
--- jmp/jmp-application/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module application {
-  requires dto;
-  requires cloud.bank.impl;
-  requires cloud.service.impl;
-
-  exports dev.msokarau.application;
-}
