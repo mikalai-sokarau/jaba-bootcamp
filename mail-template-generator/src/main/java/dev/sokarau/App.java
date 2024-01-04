0e957f62720c9ff4736cd67d272348a510fb9f3e
Mikalai Sokarau <mikalaisokarau@gmail.com>
2024-01-04 16:26:01 +0100

    feat: check for mode parameter

--- mail-template-generator/src/main/java/dev/sokarau/App.java
+++ mail-template-generator/src/main/java/dev/sokarau/App.java
@@ -4,10 +4,15 @@ package dev.sokarau;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        String mode = null;
+
+        if (args.length > 0) {
+            mode = args[0];
+        } else {
+            throw new IllegalArgumentException("Mode parameter is missing");
+        }
     }
 }
