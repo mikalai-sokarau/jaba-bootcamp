8d698bc1cfee8e5a91f925609c254869afb349ea
Mikalai Sokarau <mikalaisokarau@gmail.com>
2024-01-04 16:41:45 +0100

    test: check that launched with expected mode

--- mail-template-generator/src/test/java/dev/sokarau/AppTest.java
+++ mail-template-generator/src/test/java/dev/sokarau/AppTest.java
@@ -3,8 +3,28 @@ package dev.sokarau;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import java.util.Arrays;
+import java.util.Collection;
 
+@RunWith(Parameterized.class)
 public class AppTest {
+
+    private final String mode;
+
+    public AppTest(String mode) {
+        this.mode = mode;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+                { "file" },
+                { "console" }
+        });
+    }
 
     @Rule
     public ExpectedException exceptionRule = ExpectedException.none();
@@ -15,5 +35,12 @@ public class AppTest {
         exceptionRule.expectMessage("Mode parameter is missing");
 
         App.main(new String[0]);
+    }
+
+    @Test
+    public void shouldNotThrowErrorWhenLaunchedWithValidMode() {
+        assertDoesNotThrow(() -> {
+            App.main(new String[] { mode });
+        });
     }
 }
