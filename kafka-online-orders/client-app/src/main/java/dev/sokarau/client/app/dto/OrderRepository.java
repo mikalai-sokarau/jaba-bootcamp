05e701e486882b892eb99aef4318b611687463ac
Mikalai Sokarau <mikalaisokarau@gmail.com>
2024-02-13 16:45:07 +0100

    feat: store order status in database

--- kafka-online-orders/client-app/src/main/java/dev/sokarau/client/app/dto/OrderRepository.java
+++ kafka-online-orders/client-app/src/main/java/dev/sokarau/client/app/dto/OrderRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends MongoRepository<Order, ObjectId> {
-    Optional<Order> findOrderByOrderId(String orderId);
+    Optional<Order> findOrderByCorrelationId(String orderId);
 }
