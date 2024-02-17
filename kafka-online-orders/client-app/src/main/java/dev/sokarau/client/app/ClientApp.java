48e08ddc769b1abfc2daa733ec4ca35f7d00f7a4
Mikalai Sokarau <mikalaisokarau@gmail.com>
2024-02-17 22:17:22 +0100

    feat: use common producer and consumer configs

--- kafka-online-orders/client-app/src/main/java/dev/sokarau/client/app/ClientApp.java
+++ kafka-online-orders/client-app/src/main/java/dev/sokarau/client/app/ClientApp.java
@@ -1,13 +1,15 @@
 package dev.sokarau.client.app;
 
+import dev.sokarau.common.KafkaConsumerConfig;
+import dev.sokarau.common.KafkaProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@Import({KafkaConsumerConfig.class, KafkaProducerConfig.class})
 public class ClientApp {
-	public static void main(String[] args) {
-		SpringApplication.run(ClientApp.class, args);
-	}
+	public static void main(String[] args) { SpringApplication.run(ClientApp.class, args); }
 }
--- kafka-online-orders/courier-app/src/main/java/dev/sokarau/courier/app/CourierApp.java
+++ kafka-online-orders/courier-app/src/main/java/dev/sokarau/courier/app/CourierApp.java
@@ -1,13 +1,13 @@
 package dev.sokarau.courier.app;
 
+import dev.sokarau.common.KafkaConsumerConfig;
+import dev.sokarau.common.KafkaProducerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({KafkaConsumerConfig.class, KafkaProducerConfig.class})
 public class CourierApp {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CourierApp.class, args);
-	}
-
+	public static void main(String[] args) { SpringApplication.run(CourierApp.class, args); }
 }
